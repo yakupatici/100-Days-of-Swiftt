afaee13cd93c788f8aa80175157424efc65b87c5
Yakup Atıcı <113550359+yakupatici@users.noreply.github.com>
2023-08-30 12:36:01 +0300

    Add files via upload

---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,22 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Jacob on 30.08.2023.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var name: UILabel!
+    
+    
+    
+    
+    
+    
+}
